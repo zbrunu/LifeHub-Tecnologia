154bce59699f140ecfb4b27fabfe52819b34330b
Bruno Oliveira <brunuoliveira.alves@gmail.com>
2026-08-05 13:13:24 -0300

    Initial commit

---
+++ BackEnd/LifeHub/src/main/java/com/Bruno/LifeHub/LifeHubApplication.java
@@ -0,0 +1,13 @@
+package com.Bruno.LifeHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeHubApplication.class, args);
+	}
+
+}
---
+++ BackEnd/LifeHub/src/test/java/com/Bruno/LifeHub/LifeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bruno.LifeHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
